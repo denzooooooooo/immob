98c950592505875bf6c64a423fa704fc65661fc5
Ange Djedjed <djedjedange20@gmail.com>
2025-12-10 21:19:52 +0100

    f

--- resources/views/admin/subscriptions/index.blade.php
+++ resources/views/admin/subscriptions/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Gestion des Abonnements')
 
 @section('header', 'Gestion des Abonnements')
-
+ 
 @section('actions')
     <div class="flex space-x-3">
         <a href="{{ route('admin.subscriptions.create') }}" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700">
